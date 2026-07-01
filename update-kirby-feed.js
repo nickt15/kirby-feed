40e8ee40b536eb38bcbfc08b8fc9d5d752c3a35e
nickt15 <nickturnerths.2013@gmail.com>
2026-06-30 20:32:38 -0500

    Update update-kirby-feed.js

--- update-kirby-feed.js
+++ update-kirby-feed.js
@@ -8,8 +8,8 @@ const FEED_PATH = path.join(__dirname, "latest.json");
 const IMAGES_DIR = path.join(__dirname, "images");
 const SPECIALS_DIR = path.join(__dirname, "images", "specials");
 
-const SCAN_AHEAD = 15;
-const BACKFILL_BEHIND = 15;
+const SCAN_AHEAD = 16;
+const BACKFILL_BEHIND = 16;
 const BASE_URL = "https://codecraftsupport.com/Kirby/DATA/Images";
 const SPECIALS_PAGE = "https://codecraftsupport.com/Kirby/gallery_specials.html";
 
